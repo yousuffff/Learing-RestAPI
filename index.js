316ee3a16333357df63067b1c11b1322b594b208
Mohd Yousuf <mdyousuf43201@gmail.com>
2026-04-17 23:08:43 +0530

    Add Express server setup and EJS view rendering with initial posts data

--- index.js
+++ index.js
@@ -1,19 +1,39 @@
 import express from "express";
 import path from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
 const port = 8080;
 
+const _fileName = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(_fileName);
+let posts = [
+  {
+    username: "Yousuf",
+    content: "How's the joshsss guyzzzzzzzz",
+  },
+  {
+    username: "Abdul",
+    content: "hey Guyss meri shadhi hogyi h",
+  },
+  {
+    username: "Fahad",
+    content:
+      "hey abdul congrats BTW meri shadhi ko ek saal hone wala h . HAHAHAHAAAA",
+  },
+];
+
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-app.set(express.static(path.join(__dirname, "views")));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("hello world");
+  res.render("index.ejs", { posts });
 });
+
 app.listen(port, () => {
   console.log(`This app is running on port ${port}`);
 });
